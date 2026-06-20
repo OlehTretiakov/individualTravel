c63bef5116b3b9df684675707c156f7461136543
Ole <user@mail>
2026-06-20 17:19:31 +0300

    section other tours

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.button-next',
         prevEl: '.button-prev',
     },
-
-
 });
+
+const items = document.querySelectorAll('.tours__item')
+items.forEach(element => {
+    element.addEventListener('click', () => {
+        items.forEach(item => item.classList.remove('tours__item--active'))
+        element.classList.add('tours__item--active')
+    })
+})
